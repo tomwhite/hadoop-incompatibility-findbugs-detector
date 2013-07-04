7394b9c63a1b5da7017ad5de33b688adbfdb7142
Tom White <tom@cloudera.com>
2013-07-04 15:17:15 +0100

    Improved naming and added documentation. Included all classes that changed to interfaces in Hadoop 2.

---
+++ src/main/java/com/tom_e_white/hadoop_incompatibility_findbugs_detector/FindHadoopIncompatibleClassChange.java
@@ -0,0 +1,41 @@
+package com.tom_e_white.hadoop_incompatibility_findbugs_detector;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindHadoopIncompatibleClassChange extends OpcodeStackDetector {
+  private List<String> INCOMPATIBLE_CLASSES = Arrays.asList(new String[] {
+      // Classes changed to interfaces in https://issues.apache.org/jira/browse/MAPREDUCE-954
+      "org/apache/hadoop/mapreduce/JobContext",
+      "org/apache/hadoop/mapreduce/TaskAttemptContext",
+      "org/apache/hadoop/mapreduce/TaskInputOutputContext",
+      "org/apache/hadoop/mapreduce/MapContext",
+      "org/apache/hadoop/mapreduce/ReduceContext",
+
+      // Classes changed to interfaces in https://issues.apache.org/jira/browse/MAPREDUCE-901
+      "org/apache/hadoop/mapreduce/Counter",
+      "org/apache/hadoop/mapreduce/CounterGroup",
+  });
+
+  private BugReporter bugReporter;
+
+  public FindHadoopIncompatibleClassChange(BugReporter bugReporter) {
+    this.bugReporter = bugReporter;
+  }
+
+  @Override
+  public void sawOpcode(int seen) {
+    if ((seen == INVOKEVIRTUAL /* Hadoop 1 */ || seen == INVOKEINTERFACE /* Hadoop 2 */)
+        && INCOMPATIBLE_CLASSES.contains(getClassConstantOperand())) {
+      bugReporter.reportBug(new BugInstance(this, "HADOOP_CLASS_CHANGE_INCOMPATIBILITY_BUG",
+          NORMAL_PRIORITY)
+          .addClassAndMethod(this)
+          .addString(getDottedClassConstantOperand())
+          .addSourceLine(this));
+    }
+  }
+
+}
